1e892bbcc65eff0495a1fd3814c96d83e8409b70
Zephyr UCH-2009 <139885500+Unlimited-Crack-You@users.noreply.github.com>
2023-08-23 22:05:04 +0700

    Add files via upload

---
+++ login-tools/login-fromv1.py
@@ -0,0 +1,28 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('menghubungi server ...')
+mengetik('[-----]10%')
+mengetik('[-----]20%')
+mengetik('[-----]30%')
+mengetik('[-----]40%')
+mengetik('[-----]50%')
+mengetik('[-----]60%')
+mengetik('[-----]70%')
+mengetik('[-----]80%')
+mengetik('[-----]90%')
+mengetik('[-----]100%')
+mengetik('Membuka file Script mohon tunggu')
+mengetik('membuka server negara mohon tunggu......')
+mengetik('[-----]10%')
+mengetik('[-----]35%')
+mengetik('[-----]60%')
+mengetik('[-----]80%')
+mengetik('[-----]100%')
+mengetik('server negara telah di dapat')
+mengetik('Loading...Done')
