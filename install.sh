bed1a6fce45db88bf6df13be5b38e3978b09dd48
Zephyr UCH-2009 <139885500+Unlimited-Crack-You@users.noreply.github.com>
2023-08-29 21:19:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -263,6 +263,215 @@ sleep 2s
 echo "~{${ORANGE} Bersiap Untuk Menginstal Tuan ${GREEN}}~"
 sleep 3s
 echo " "
+echo "${ORANGE} Loading .......✓"
+
+echo -ne " Loading [1]\r"
+sleep 0
+echo -ne " Loading [2]\r"
+sleep 0
+echo -ne " Loading [3]\r"
+sleep 0
+echo -ne " Loading [4]\r"
+sleep 0
+echo -ne " Loading [5]\r"
+sleep 0
+echo -ne " Loading [6]\r"
+sleep 0
+echo -ne " Loading [7]\r"
+sleep 0
+echo -ne " Loading [8]\r"
+sleep 0
+echo -ne " Loading [9]\r"
+sleep 0
+echo -ne " Loading [10]\r"
+sleep 0
+echo -ne " Loading [11]\r"
+sleep 0
+echo -ne " Loading [12]\r"
+sleep 0
+echo -ne " Loading [13]\r"
+sleep 0
+echo -ne " Loading [14]\r"
+sleep 0
+echo -ne " Loading [15]\r"
+sleep 0
+echo -ne " Loading [16]\r"
+sleep 0
+echo -ne " Loading [17]\r"
+sleep 0
+echo -ne " Loading [18]\r"
+sleep 0
+echo -ne " Loading [19]\r"
+sleep 0
+echo -ne " Loading [20]\r"
+sleep 0
+echo -ne " Loading [21]\r"
+sleep 0
+echo -ne " Loading [22]\r"
+sleep 0
+echo -ne " Loading [23]\r"
+sleep 0
+echo -ne " Loading [24]\r"
+sleep 0
+echo -ne " Loading [25]\r"
+sleep 0
+echo -ne " Loading [26]\r"
+sleep 0
+echo -ne " Loading [27]\r"
+sleep 0
+echo -ne " Loading [28]\r"
+sleep 0
+echo -ne " Loading [29]\r"
+sleep 0
+echo -ne " Loading [30]\r"
+sleep 0
+echo -ne " Loading [31]\r"
+sleep 0
+echo -ne " Loading [32]\r"
+sleep 0
+echo -ne " Loading [33]\r"
+sleep 0
+echo -ne " Loading [34]\r"
+sleep 0
+echo -ne " Loading [35]\r"
+sleep 0
+echo -ne " Loading [36]\r"
+sleep 0
+echo -ne " Loading [37]\r"
+sleep 0
+echo -ne " Loading [38]\r"
+sleep 0
+echo -ne " Loading [39]\r"
+sleep 0
+echo -ne " Loading [40]\r"
+sleep 0
+echo -ne " Loading [41]\r"
+sleep 0
+echo -ne " Loading [42]\r"
+sleep 0
+echo -ne " Loading [43]\r"
+sleep 0
+echo -ne " Loading [44]\r"
+sleep 0
+echo -ne " Loading [45]\r"
+sleep 0
+echo -ne " Loading [46]\r"
+sleep 0
+echo -ne " Loading [47]\r"
+sleep 0
+echo -ne " Loading [48]\r"
+sleep 0
+echo -ne " Loading [49]\r"
+sleep 0
+echo -ne " Loading [50]\r"
+sleep 0
+echo -ne " Loading [51]\r"
+sleep 0
+echo -ne " Loading [52]\r"
+sleep 0
+echo -ne " Loading [53]\r"
+sleep 0
+echo -ne " Loading [54]\r"
+sleep 0
+echo -ne " Loading [55]\r"
+sleep 0
+echo -ne " Loading [56]\r"
+sleep 0
+echo -ne " Loading [57]\r"
+sleep 0
+echo -ne " Loading [58]\r"
+sleep 0
+echo -ne " Loading [59]\r"
+sleep 0
+echo -ne " Loading [60]\r"
+sleep 0
+echo -ne " Loading [61]\r"
+sleep 0
+echo -ne " Loading [62]\r"
+sleep 0
+echo -ne " Loading [63]\r"
+sleep 0
+echo -ne " Loading [64]\r"
+sleep 0
+echo -ne " Loading [65]\r"
+sleep 0
+echo -ne " Loading [66]\r"
+sleep 0
+echo -ne " Loading [67]\r"
+sleep 0
+echo -ne " Loading [68]\r"
+sleep 0
+echo -ne " Loading [69]\r"
+sleep 0
+echo -ne " Loading [70]\r"
+sleep 0
+echo -ne " Loading [71]\r"
+sleep 0
+echo -ne " Loading [72]\r"
+sleep 0
+echo -ne " Loading [73]\r"
+sleep 0
+echo -ne " Loading [74]\r"
+sleep 0
+echo -ne " Loading [75]\r"
+sleep 0
+echo -ne " Loading [76]\r"
+sleep 0
+echo -ne " Loading [77]\r"
+sleep 0
+echo -ne " Loading [78]\r"
+sleep 0
+echo -ne " Loading [75]\r"
+sleep 0
+echo -ne " Loading [76]\r"
+sleep 0
+echo -ne " Loading [77]\r"
+sleep 0
+echo -ne " Loading [78]\r"
+sleep 0
+echo -ne " Loading [79]\r"
+sleep 0
+echo -ne " Loading [80]\r"
+sleep 0
+echo -ne " Loading [81]\r"
+sleep 0
+echo -ne " Loading [82]\r"
+sleep 0
+echo -ne " Loading [83]\r"
+sleep 0
+echo -ne " Loading [84]\r"
+sleep 0
+echo -ne " Loading [85]\r"
+sleep 0
+echo -ne " Loading [86]\r"
+sleep 0
+echo -ne " Loading [87]\r"
+sleep 0
+echo -ne " Loading [88]\r"
+sleep 0
+echo -ne " Loading [89]\r"
+sleep 0
+echo -ne " Loading [90]\r"
+sleep 0
+echo -ne " Loading [91]\r"
+sleep 0
+echo -ne " Loading [92]\r"
+sleep 0
+echo -ne " Loading [93]\r"
+sleep 0
+echo -ne " Loading [94]\r"
+sleep 0
+echo -ne " Loading [95]\r"
+sleep 0
+echo -ne " Loading [96]\r"
+sleep 0
+echo -ne " Loading [97]\r"
+sleep 0
+echo -ne " Loading [98]\r"
+sleep 0
+echo -ne " Loading [99]\r"
+sleep 0
+echo -ne " Loading [100]\r"
          
 pkg upgrade && pkg update
 pkg install git
@@ -306,6 +515,215 @@ sleep 2s
 echo "~{${ORANGE} Bersiap Untuk Menginstal Tuan }~"
 sleep 3s
 echo " "
+echo "${ORANGE} Loading .......✓"
+
+echo -ne " Loading [1]\r"
+sleep 0
+echo -ne " Loading [2]\r"
+sleep 0
+echo -ne " Loading [3]\r"
+sleep 0
+echo -ne " Loading [4]\r"
+sleep 0
+echo -ne " Loading [5]\r"
+sleep 0
+echo -ne " Loading [6]\r"
+sleep 0
+echo -ne " Loading [7]\r"
+sleep 0
+echo -ne " Loading [8]\r"
+sleep 0
+echo -ne " Loading [9]\r"
+sleep 0
+echo -ne " Loading [10]\r"
+sleep 0
+echo -ne " Loading [11]\r"
+sleep 0
+echo -ne " Loading [12]\r"
+sleep 0
+echo -ne " Loading [13]\r"
+sleep 0
+echo -ne " Loading [14]\r"
+sleep 0
+echo -ne " Loading [15]\r"
+sleep 0
+echo -ne " Loading [16]\r"
+sleep 0
+echo -ne " Loading [17]\r"
+sleep 0
+echo -ne " Loading [18]\r"
+sleep 0
+echo -ne " Loading [19]\r"
+sleep 0
+echo -ne " Loading [20]\r"
+sleep 0
+echo -ne " Loading [21]\r"
+sleep 0
+echo -ne " Loading [22]\r"
+sleep 0
+echo -ne " Loading [23]\r"
+sleep 0
+echo -ne " Loading [24]\r"
+sleep 0
+echo -ne " Loading [25]\r"
+sleep 0
+echo -ne " Loading [26]\r"
+sleep 0
+echo -ne " Loading [27]\r"
+sleep 0
+echo -ne " Loading [28]\r"
+sleep 0
+echo -ne " Loading [29]\r"
+sleep 0
+echo -ne " Loading [30]\r"
+sleep 0
+echo -ne " Loading [31]\r"
+sleep 0
+echo -ne " Loading [32]\r"
+sleep 0
+echo -ne " Loading [33]\r"
+sleep 0
+echo -ne " Loading [34]\r"
+sleep 0
+echo -ne " Loading [35]\r"
+sleep 0
+echo -ne " Loading [36]\r"
+sleep 0
+echo -ne " Loading [37]\r"
+sleep 0
+echo -ne " Loading [38]\r"
+sleep 0
+echo -ne " Loading [39]\r"
+sleep 0
+echo -ne " Loading [40]\r"
+sleep 0
+echo -ne " Loading [41]\r"
+sleep 0
+echo -ne " Loading [42]\r"
+sleep 0
+echo -ne " Loading [43]\r"
+sleep 0
+echo -ne " Loading [44]\r"
+sleep 0
+echo -ne " Loading [45]\r"
+sleep 0
+echo -ne " Loading [46]\r"
+sleep 0
+echo -ne " Loading [47]\r"
+sleep 0
+echo -ne " Loading [48]\r"
+sleep 0
+echo -ne " Loading [49]\r"
+sleep 0
+echo -ne " Loading [50]\r"
+sleep 0
+echo -ne " Loading [51]\r"
+sleep 0
+echo -ne " Loading [52]\r"
+sleep 0
+echo -ne " Loading [53]\r"
+sleep 0
+echo -ne " Loading [54]\r"
+sleep 0
+echo -ne " Loading [55]\r"
+sleep 0
+echo -ne " Loading [56]\r"
+sleep 0
+echo -ne " Loading [57]\r"
+sleep 0
+echo -ne " Loading [58]\r"
+sleep 0
+echo -ne " Loading [59]\r"
+sleep 0
+echo -ne " Loading [60]\r"
+sleep 0
+echo -ne " Loading [61]\r"
+sleep 0
+echo -ne " Loading [62]\r"
+sleep 0
+echo -ne " Loading [63]\r"
+sleep 0
+echo -ne " Loading [64]\r"
+sleep 0
+echo -ne " Loading [65]\r"
+sleep 0
+echo -ne " Loading [66]\r"
+sleep 0
+echo -ne " Loading [67]\r"
+sleep 0
+echo -ne " Loading [68]\r"
+sleep 0
+echo -ne " Loading [69]\r"
+sleep 0
+echo -ne " Loading [70]\r"
+sleep 0
+echo -ne " Loading [71]\r"
+sleep 0
+echo -ne " Loading [72]\r"
+sleep 0
+echo -ne " Loading [73]\r"
+sleep 0
+echo -ne " Loading [74]\r"
+sleep 0
+echo -ne " Loading [75]\r"
+sleep 0
+echo -ne " Loading [76]\r"
+sleep 0
+echo -ne " Loading [77]\r"
+sleep 0
+echo -ne " Loading [78]\r"
+sleep 0
+echo -ne " Loading [75]\r"
+sleep 0
+echo -ne " Loading [76]\r"
+sleep 0
+echo -ne " Loading [77]\r"
+sleep 0
+echo -ne " Loading [78]\r"
+sleep 0
+echo -ne " Loading [79]\r"
+sleep 0
+echo -ne " Loading [80]\r"
+sleep 0
+echo -ne " Loading [81]\r"
+sleep 0
+echo -ne " Loading [82]\r"
+sleep 0
+echo -ne " Loading [83]\r"
+sleep 0
+echo -ne " Loading [84]\r"
+sleep 0
+echo -ne " Loading [85]\r"
+sleep 0
+echo -ne " Loading [86]\r"
+sleep 0
+echo -ne " Loading [87]\r"
+sleep 0
+echo -ne " Loading [88]\r"
+sleep 0
+echo -ne " Loading [89]\r"
+sleep 0
+echo -ne " Loading [90]\r"
+sleep 0
+echo -ne " Loading [91]\r"
+sleep 0
+echo -ne " Loading [92]\r"
+sleep 0
+echo -ne " Loading [93]\r"
+sleep 0
+echo -ne " Loading [94]\r"
+sleep 0
+echo -ne " Loading [95]\r"
+sleep 0
+echo -ne " Loading [96]\r"
+sleep 0
+echo -ne " Loading [97]\r"
+sleep 0
+echo -ne " Loading [98]\r"
+sleep 0
+echo -ne " Loading [99]\r"
+sleep 0
+echo -ne " Loading [100]\r"
     
 apt upgrade && pkg update
 apt install git
@@ -349,6 +767,215 @@ sleep 2s
 echo "~{${ORANGE} Bersiap Untuk Menginstal Tuan }~" 
 sleep 3s
 echo " "
+echo "${ORANGE} Loading .......✓"
+
+echo -ne " Loading [1]\r"
+sleep 0
+echo -ne " Loading [2]\r"
+sleep 0
+echo -ne " Loading [3]\r"
+sleep 0
+echo -ne " Loading [4]\r"
+sleep 0
+echo -ne " Loading [5]\r"
+sleep 0
+echo -ne " Loading [6]\r"
+sleep 0
+echo -ne " Loading [7]\r"
+sleep 0
+echo -ne " Loading [8]\r"
+sleep 0
+echo -ne " Loading [9]\r"
+sleep 0
+echo -ne " Loading [10]\r"
+sleep 0
+echo -ne " Loading [11]\r"
+sleep 0
+echo -ne " Loading [12]\r"
+sleep 0
+echo -ne " Loading [13]\r"
+sleep 0
+echo -ne " Loading [14]\r"
+sleep 0
+echo -ne " Loading [15]\r"
+sleep 0
+echo -ne " Loading [16]\r"
+sleep 0
+echo -ne " Loading [17]\r"
+sleep 0
+echo -ne " Loading [18]\r"
+sleep 0
+echo -ne " Loading [19]\r"
+sleep 0
+echo -ne " Loading [20]\r"
+sleep 0
+echo -ne " Loading [21]\r"
+sleep 0
+echo -ne " Loading [22]\r"
+sleep 0
+echo -ne " Loading [23]\r"
+sleep 0
+echo -ne " Loading [24]\r"
+sleep 0
+echo -ne " Loading [25]\r"
+sleep 0
+echo -ne " Loading [26]\r"
+sleep 0
+echo -ne " Loading [27]\r"
+sleep 0
+echo -ne " Loading [28]\r"
+sleep 0
+echo -ne " Loading [29]\r"
+sleep 0
+echo -ne " Loading [30]\r"
+sleep 0
+echo -ne " Loading [31]\r"
+sleep 0
+echo -ne " Loading [32]\r"
+sleep 0
+echo -ne " Loading [33]\r"
+sleep 0
+echo -ne " Loading [34]\r"
+sleep 0
+echo -ne " Loading [35]\r"
+sleep 0
+echo -ne " Loading [36]\r"
+sleep 0
+echo -ne " Loading [37]\r"
+sleep 0
+echo -ne " Loading [38]\r"
+sleep 0
+echo -ne " Loading [39]\r"
+sleep 0
+echo -ne " Loading [40]\r"
+sleep 0
+echo -ne " Loading [41]\r"
+sleep 0
+echo -ne " Loading [42]\r"
+sleep 0
+echo -ne " Loading [43]\r"
+sleep 0
+echo -ne " Loading [44]\r"
+sleep 0
+echo -ne " Loading [45]\r"
+sleep 0
+echo -ne " Loading [46]\r"
+sleep 0
+echo -ne " Loading [47]\r"
+sleep 0
+echo -ne " Loading [48]\r"
+sleep 0
+echo -ne " Loading [49]\r"
+sleep 0
+echo -ne " Loading [50]\r"
+sleep 0
+echo -ne " Loading [51]\r"
+sleep 0
+echo -ne " Loading [52]\r"
+sleep 0
+echo -ne " Loading [53]\r"
+sleep 0
+echo -ne " Loading [54]\r"
+sleep 0
+echo -ne " Loading [55]\r"
+sleep 0
+echo -ne " Loading [56]\r"
+sleep 0
+echo -ne " Loading [57]\r"
+sleep 0
+echo -ne " Loading [58]\r"
+sleep 0
+echo -ne " Loading [59]\r"
+sleep 0
+echo -ne " Loading [60]\r"
+sleep 0
+echo -ne " Loading [61]\r"
+sleep 0
+echo -ne " Loading [62]\r"
+sleep 0
+echo -ne " Loading [63]\r"
+sleep 0
+echo -ne " Loading [64]\r"
+sleep 0
+echo -ne " Loading [65]\r"
+sleep 0
+echo -ne " Loading [66]\r"
+sleep 0
+echo -ne " Loading [67]\r"
+sleep 0
+echo -ne " Loading [68]\r"
+sleep 0
+echo -ne " Loading [69]\r"
+sleep 0
+echo -ne " Loading [70]\r"
+sleep 0
+echo -ne " Loading [71]\r"
+sleep 0
+echo -ne " Loading [72]\r"
+sleep 0
+echo -ne " Loading [73]\r"
+sleep 0
+echo -ne " Loading [74]\r"
+sleep 0
+echo -ne " Loading [75]\r"
+sleep 0
+echo -ne " Loading [76]\r"
+sleep 0
+echo -ne " Loading [77]\r"
+sleep 0
+echo -ne " Loading [78]\r"
+sleep 0
+echo -ne " Loading [75]\r"
+sleep 0
+echo -ne " Loading [76]\r"
+sleep 0
+echo -ne " Loading [77]\r"
+sleep 0
+echo -ne " Loading [78]\r"
+sleep 0
+echo -ne " Loading [79]\r"
+sleep 0
+echo -ne " Loading [80]\r"
+sleep 0
+echo -ne " Loading [81]\r"
+sleep 0
+echo -ne " Loading [82]\r"
+sleep 0
+echo -ne " Loading [83]\r"
+sleep 0
+echo -ne " Loading [84]\r"
+sleep 0
+echo -ne " Loading [85]\r"
+sleep 0
+echo -ne " Loading [86]\r"
+sleep 0
+echo -ne " Loading [87]\r"
+sleep 0
+echo -ne " Loading [88]\r"
+sleep 0
+echo -ne " Loading [89]\r"
+sleep 0
+echo -ne " Loading [90]\r"
+sleep 0
+echo -ne " Loading [91]\r"
+sleep 0
+echo -ne " Loading [92]\r"
+sleep 0
+echo -ne " Loading [93]\r"
+sleep 0
+echo -ne " Loading [94]\r"
+sleep 0
+echo -ne " Loading [95]\r"
+sleep 0
+echo -ne " Loading [96]\r"
+sleep 0
+echo -ne " Loading [97]\r"
+sleep 0
+echo -ne " Loading [98]\r"
+sleep 0
+echo -ne " Loading [99]\r"
+sleep 0
+echo -ne " Loading [100]\r"
 
 sudo apt-get upgrade && pkg update
 sudo apt-get install git
